0035c39f66998851767b0a391b8e260975f9dfbc
ShiaoLin <45435509+ShiaoLin@users.noreply.github.com>
2022-04-05 21:54:02 +0800

    Delete .modules.order.cmd

--- quiz8/ptrace/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/shiaoyi/linux2022/linux2022/quiz8/ptrace/modules.order := {   echo /home/shiaoyi/linux2022/linux2022/quiz8/ptrace/dont_trace.ko; :; } | awk '!x[$$0]++' - > /home/shiaoyi/linux2022/linux2022/quiz8/ptrace/modules.order
